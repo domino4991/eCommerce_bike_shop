0eae035ac719d9ff7df5649bd4f615d806e55211
Kochkin Vladislav <ltrickfaim@gmail.com>
2018-12-19 15:54:00 +0600

    7 elements is ready

--- app/js/common.js
+++ app/js/common.js
@@ -1,20 +1,7 @@
 $(function() {
-  $('.owl-carousel').owlCarousel({
-    loop: true,
-    dots: true,
-    dotsContainer: '.dots',
-    nav: false,
-    navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
-    items: 1,
-    animateOut: 'zoomOut',
-    animateIn: 'zoomIn',
-    navSpeed: 2000,
-    responsive: {
-      800: {
-        nav: true
-      }
-    }
-  })
+  // $('.owl-carousel').owlCarousel({
+    
+  // })
 
   $('.hamburger').on('click', function() {
     $(this).toggleClass('is-active');
@@ -26,6 +13,14 @@ $(function() {
     $('.hamburger').removeClass('is-active');
     $('.nav').removeClass('nav-active');
     $('body').removeClass('fixed');
+  })
+
+  $('.addWishlist').on('click', function(){
+    $('.fa-wish').toggleClass('activeWish');
+  })
+
+  $('.tabs__link').on('click', function(){
+    $(this).toggleClass('activeTabs');
   })
 
   $('.flowing-scroll').on( 'click', function(){ 
@@ -40,60 +35,23 @@ $(function() {
   return false;
   });
 
-  let target = $('.tour');
-  let targetPos = target.offset().top;
-  let winHeight = $(window).height();
-  let scrollToElem = targetPos - winHeight;
-  $(window).scroll(function(){
-    var winScrollTop = $(this).scrollTop();
-    if(winScrollTop > scrollToElem){
-      $('.scroll-top').css({
-        display: 'block'
-      });
-    } else {
-      $('.scroll-top').css({
-            display: 'none'
-      });
-    }
-  });
-    let map;
-
-    DG.then(function () {
-        map = DG.map('map', {
-            center: [42.842088, 74.621858],
-            zoom: 16,
-            dragging : true,
-            touchZoom: true,
-            scrollWheelZoom: false,
-            doubleClickZoom: false,
-            boxZoom: false,
-            geoclicker: false,
-            zoomControl: true,
-            fullscreenControl: true
-        });
-
-        DG.marker([42.842088, 74.621858]).addTo(map).bindPopup('Hiking Travel').bindLabel('Hiking Travel', {static: true});
-
-    });
+  // let target = $('.tour');
+  // let targetPos = target.offset().top;
+  // let winHeight = $(window).height();
+  // let scrollToElem = targetPos - winHeight;
+  // $(window).scroll(function(){
+  //   var winScrollTop = $(this).scrollTop();
+  //   if(winScrollTop > scrollToElem){
+  //     $('.scroll-top').css({
+  //       display: 'block'
+  //     });
+  //   } else {
+  //     $('.scroll-top').css({
+  //           display: 'none'
+  //     });
+  //   }
+  // });
     
-    $("#phone").mask("+999 (999) 999-999");
-    new WOW().init();
-
-    $('.events__item--front').on('click', function() {
-      $(this).css({
-        transform: 'rotateY(180deg)'
-      });
-      $(this).next().css({
-        transform: 'rotateY(360deg)'
-      })
-    });
-
-    $('.events__item--back').on('click', function() {
-      $(this).css({
-        transform: 'rotateY(180deg)'
-      });
-      $(this).prev().css({
-        transform: 'rotateY(0deg)'
-      })
-    });
+    // $("#phone").mask("+999 (999) 999-999");
+    // new WOW().init();
 });
